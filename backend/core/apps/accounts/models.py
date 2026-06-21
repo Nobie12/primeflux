8855e0242f7ed6750ab4dab70945ddef37cbd181
Ken Aule <kenaule1@gmail.com>
2026-06-22 00:37:23 +0300

    Introduced independent created_at/updated_at tracking metrics to isolate the onboarding timeline from user account sign-up

--- backend/core/apps/accounts/models.py
+++ backend/core/apps/accounts/models.py
@@ -68,6 +68,9 @@ class Driver(models.Model):
     )
     is_verified = models.BooleanField(default=False)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     objects = DriverManager()
 
     def __str__(self):
